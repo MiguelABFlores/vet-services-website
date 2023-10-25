3973ad2cb329a7af29b0e7cfbae0c4eb6566b130
Miguel Angel Briseño Flores <miiguelb07@gmail.com>
2023-10-25 16:14:33 -0600

    updated links for the images references

--- src/js/project.js
+++ src/js/project.js
@@ -1,4 +1,8 @@
 import '../css/style.css';
+import sveLogo from '../public/assets/SVE-logo.ico';
+
+var homeImg = document.getElementById('logo');
+homeImg.src = sveLogo;
 
 function revealMessage() {
     document.getElementById("hiddenMessage").style.display = 'block';

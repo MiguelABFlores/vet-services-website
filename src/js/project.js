038dc27ab75aa24c002da3e77a719f2b7c78b1dd
jonathan <jonathan.gyek@gmail.com>
2023-10-26 00:09:57 -0600

    added a new import to about image

--- src/js/project.js
+++ src/js/project.js
@@ -1,8 +1,11 @@
 import '../css/style.css';
 import sveLogo from '../public/assets/SVE-logo.ico';
 import homeImage from '../public/assets/img-home-page.jpg';
+import aboutImage from '../public/assets/about-img.jpg';
 
 var homeLogo = document.getElementById('logo');
 var homeImg = document.getElementById('homeImg');
+var aboutImg = document.getElementById('aboutImg');
 homeLogo.src = sveLogo;
 homeImg.src = homeImage;
+aboutImg.src = aboutImage;

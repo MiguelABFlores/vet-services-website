455ebab20734ae6adc602b291704638e4e838acb
jonathan <jonathan.gyek@gmail.com>
2023-10-26 22:29:28 -0600

    modified project.js with a new image for services section and modified html img element.

--- src/js/project.js
+++ src/js/project.js
@@ -2,10 +2,13 @@ import '../css/style.css';
 import sveLogo from '../public/assets/SVE-logo.ico';
 import homeImage from '../public/assets/img-home-page.jpg';
 import aboutImage from '../public/assets/about-img.jpg';
+import servicesImg from '../public/assets/costo-distancia.jpeg'
 
 var homeLogo = document.getElementById('logo');
 var homeImg = document.getElementById('homeImg');
 var aboutImg = document.getElementById('aboutImg');
+var servicesImg = document.getElementById('servicesImg')
 homeLogo.src = sveLogo;
 homeImg.src = homeImage;
 aboutImg.src = aboutImage;
+servicesImg.src = servicesImg;

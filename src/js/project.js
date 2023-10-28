e582ba517694346a01efdfc4ab02763b65488b9a
jonathan <jonathan.gyek@gmail.com>
2023-10-27 21:54:10 -0600

    added a js variable for contact image, deleted one image from assets and changed id image in html

--- src/js/project.js
+++ src/js/project.js
@@ -3,12 +3,15 @@ import sveLogo from '../public/assets/SVE-logo.ico';
 import homeImage from '../public/assets/img-home-page.jpg';
 import aboutImage from '../public/assets/about-img.jpg';
 import servicesImage from '../public/assets/costo-distancia.jpeg'
+import contactImage from '../public/assets/contact-img.png'
 
 var homeLogo = document.getElementById('logo');
 var homeImg = document.getElementById('homeImg');
 var aboutImg = document.getElementById('aboutImg');
 var servicesImg = document.getElementById('servicesImg')
+var contactImg = document.getElementById('contactImg')
 homeLogo.src = sveLogo;
 homeImg.src = homeImage;
 aboutImg.src = aboutImage;
 servicesImg.src = servicesImage;
+contactImg.src = contactImage;

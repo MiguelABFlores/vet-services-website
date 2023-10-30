49c093be6914a98f9af1282d9d0b1cc917647d73
jonathan <jonathan.gyek@gmail.com>
2023-10-29 23:28:26 -0600

    create a var footerImg in js

--- src/js/project.js
+++ src/js/project.js
@@ -4,6 +4,7 @@ import homeImage from '../public/assets/img-home-page.jpg';
 import aboutImage from '../public/assets/about-img.jpg';
 import servicesImage from '../public/assets/costo-distancia.jpeg'
 import contactImage from '../public/assets/contact-img.png'
+import footerImage from  '../public/assets/footer-logo.ico'
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     var homeLogo = document.getElementById('logo');
@@ -11,9 +12,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var aboutImg = document.getElementById('aboutImg');
     var servicesImg = document.getElementById('servicesImg')
     var contactImg = document.getElementById('contactImg')
+    var footerImg = document.getElementById('footerImg')
     homeLogo.src = sveLogo;
     homeImg.src = homeImage;
     aboutImg.src = aboutImage;
     servicesImg.src = servicesImage;
     contactImg.src = contactImage;
+    footerImg.src = footerImage
 });

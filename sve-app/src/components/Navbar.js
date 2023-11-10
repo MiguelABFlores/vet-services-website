6e070d829f26de4e2169c5d899d2065ad4622463
jonathan <jonathan.gyek@gmail.com>
2023-11-09 23:25:26 -0600

    small adjustments

--- sve-app/src/components/Navbar.js
+++ sve-app/src/components/Navbar.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import images from '../assets/images/images';
-import '../components/Navbar.css';
+import '../styles/Navbar.css';
 
 function Navbar() {
   return (
     <div>
-      <header className="header">
+      <div className="header">
         <Link to="/">
           <img src={images.logo} className="logo" alt="SVE-logo" />
         </Link>
 
-        <nav className="navbar">
-          <a href="#home">Home</a>
-          <a href="#about">Quiénes somos</a>
-          <a href="#services">Servicios</a>
-          <a href="#contact">Contacto</a>
-        </nav>
-      </header>
+        <div className="navbar">
+          <Link to='/'>Home</Link>
+          <Link to='/about'>Quienes somos</Link>
+          <Link to='/services'>Servicios</Link>
+          <Link to='/contact'>Contactanos</Link>
+        </div>
+      </div>
     </div>
   );
 }
--- sve-app/src/pages/Home.js
+++ sve-app/src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
-
+import '../styles/Home.css'
 
 function Home() {
   return (
-    <div>
-      <div>Home Page</div>
+    <div className="home">
+      <video src="../assets/videos/home-video.mp4"></video>
+      <h1 className="">Servicios Veterinarios Eläin</h1>
+      <p>lorem ipsum</p>
+      <div>
+
+      </div>
     </div>
   );
 }

d7c64af600c752cf3e9dd2d153cbae905562309d
jonathan <jonathan.gyek@gmail.com>
2023-11-10 19:44:31 -0600

    include webpage sections

--- sve-app/src/App.js
+++ sve-app/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./App.css";
-import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Doctors from "./pages/Doctors";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home/>
+      <Home />
+      <About />
+      <Doctors />
+      <Services />
+      <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ sve-app/src/pages/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/About.css";
+
+function About() {
+  return <div>About</div>;
+}
+
+export default About;

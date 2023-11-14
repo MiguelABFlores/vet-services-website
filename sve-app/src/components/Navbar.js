b61b94cfb70a9e80b0fbfc59b1a8b4f51a1299a4
jonathan <jonathan.gyek@gmail.com>
2023-11-14 00:06:51 -0600

    react

--- sve-app/src/components/Navbar.js
+++ sve-app/src/components/Navbar.js
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa"; // Importa los íconos necesarios
 import images from "../assets/images/images";
 import "../styles/Navbar.css";
 
 function Navbar() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
-    <div>
-      <div className="header">
-        <a href="#home">
-          <img src={images.logo} className="logo" alt="SVE-logo" />
+    <header className="header">
+      <a href="#home">
+        <img src={images.logo} className="logo" alt="SVE-logo" />
+      </a>
+
+      <nav className={`navbar ${isMenuOpen ? "open" : ""}`}>
+        <a href="#home" className="active" onClick={closeMenu}>
+          Home
+        </a>
+        <a href="#about" onClick={closeMenu}>
+          Quienes somos
+        </a>
+        <a href="#services" onClick={closeMenu}>
+          Servicios
+        </a>
+        <a href="#contact" onClick={closeMenu}>
+          Contactanos
         </a>
 
-        <nav className="navbar">
-          <a href="#home" to="/">
-            Home
-          </a>
-          <a href="#about">Quienes somos</a>
-          <a href="#services">Servicios</a>
-          <a href="#contact">Contactanos</a>
-        </nav>
-      </div>
-    </div>
+        <div className="mobile" onClick={toggleMenu}>
+          {isMenuOpen ? <FaTimes /> : <FaBars />}
+        </div>
+      </nav>
+    </header>
   );
 }
 

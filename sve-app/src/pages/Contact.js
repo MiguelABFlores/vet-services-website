8bc03841994933fe99221dffe7bd8f946c5879a9
jonathan <jonathan.gyek@gmail.com>
2023-11-30 20:27:41 -0600

    adjust responsiveness in contact and about section

--- sve-app/src/pages/Contact.js
+++ sve-app/src/pages/Contact.js
@@ -9,20 +9,6 @@ function Contact() {
           <h2>Contacto</h2>
           <p>¿Tienes una duda o pregunta?</p>
           <p>¡Mándanos mensaje en unas de nuestras redes sociales!</p>
-          <a
-            href="https://www.instagram.com/svelain/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="bi bi-whatsapp"/>
-          </a>
-          <a
-            href="https://api.whatsapp.com/send?phone=523317654147"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="bi bi-whatsapp"/>
-          </a>
         </div>
 
         <div class="contact-image">
